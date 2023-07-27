fc31abf5bcff7fdf7eedf30aaea29af73d3cbfa0
gonzalez  ramon nicolas <nicolsgonza765@gmail.com>
2023-07-27 17:17:42 -0300

    version 1.3

--- src/main/resources/static/assets/script/manager.js
+++ src/main/resources/static/assets/script/manager.js
@@ -3,17 +3,39 @@ const {createApp} = Vue;
 const app = createApp({
     data(){
         return{
-
+            nombre:"",
+            descripcion: "",
+            precio:"",
+            categoria: "",
+            stock: null,
+            file: "",
         }
     },
     created(){
 
     },
+    mounted() {
+        new Cleave('#amount', {
+          numeral: true,
+          numeralThousandsGroupStyle: 'thousand',
+          prefix: '$'
+        });
+      },
     methods:{
+        createdProduct(){
 
+        }
     },
     computed:{
 
     },
 })
 app.mount("#app")
+
+// this.precio = document.querySelector('#precio').value;
+
+// // Elimina los caracteres no numéricos
+// this.precio = this.precio.replace(/[^0-9]/g, '');
+
+// // Convierte la cadena a un número
+// this.precio = parseInt(this.precio);
