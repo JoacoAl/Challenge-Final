e4a1068eb649cf874aea84acf1fddcfa0d40a576
Eduardo Oriolani <lemarch@MacBook-Pro-de-Eduardo.local>
2023-07-29 11:01:22 -0300

    haciendo categorias

--- src/main/resources/static/assets/script/productos.js
+++ src/main/resources/static/assets/script/productos.js
@@ -2,18 +2,34 @@ window.addEventListener("scroll", function() {
     const navbar = document.getElementById("navbar");
     const scrollPosition = window.scrollY;
     const navbarHeight = navbar.offsetHeight;
-    const headerHeight = 500; 
+    const headerHeight = 300; 
 
     const opacity = Math.min(1, scrollPosition / (headerHeight - navbarHeight));
     if (scrollPosition > headerHeight) {
-        navbar.classList.add("scrolled");
+        navbar.classList.add("top-nav");
+        navbar.classList.remove("navbar-interno_home")
       } else {
-        navbar.classList.remove("scrolled");
+        navbar.classList.remove("top-nav");
+        navbar.classList.add("navbar-interno_home")
       }
-    // Establece el fondo del nav con el nivel de opacidad calculado
     navbar.style.backgroundColor = `rgba(0, 0, 0, ${opacity})`;
   });
 
+const {createApp} = Vue
+
+createApp({
+  data() {
+    return {
+      
+    };
+  },
+  created(){
+
+  },
+  methods: {
+    
+  },
+}).mount("#app")
 
 
 
