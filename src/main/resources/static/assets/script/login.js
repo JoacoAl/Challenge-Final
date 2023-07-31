e50832134fa8e269b6b4dc6d1e1f9fd95b404361
Eduardo Oriolani <lemarch@MacBook-Pro-de-Eduardo.local>
2023-07-31 17:45:51 -0300

    login creado

--- src/main/resources/static/assets/script/login.js
+++ src/main/resources/static/assets/script/login.js
@@ -21,11 +21,11 @@ createApp({
                 .then(response => {
                   console.log(response);
                   Swal.fire(
-                    'Login exitoso!',
+                    'Bienvenidx a Gozo!',
                     'Ahora podés realizar compras.',
                     'success'
                   ).then(() => {
-                    window.location.href = "/assets/tabacos.html";
+                    window.location.href = "/index.html";
                   });
                 })
                 .catch(error => {
@@ -39,6 +39,40 @@ createApp({
                   this.password = "";
                 });
         },
+        registro(){
+            axios.post('/api/clientes','nombre='+ this.nombre +'&apellido='+ this.apellido +'&email=' + this.email +'&contraseña='+ this.contraseña + '&telefono=' + this.telefono + '&direccion=' + this.direccion,{headers:{'content-type':'application/x-www-form-urlencoded'}})
+                .then(response => {
+                  console.log(response);
+                  Swal.fire({
+                    title: 'Registro exitoso!',
+                    text: 'Espera a ser logueado y podes comprar lo que quieras!.',
+                    timer: 2000,
+                    showConfirmButton: false,
+                    timerProgressBar: true,
+                    onBeforeOpen: () => {
+                      Swal.showLoading();
+                    }
+                  }).then(() => {
+                    this.ingreso();
+                  });
+                })
+                .catch(error => {
+                  console.log(error.response);
+                      Swal.fire(
+                      'Registro falló!',
+                      error.response.data,
+                      'error'
+                    );
+                    this.nombre = "";
+                    this.apellido = "";
+                    this.email = "";
+                    this.contraseña = "";
+                    this.telefono = "";
+                    this.direccion = "";
+                })
+      
+          },
+
         mostrarRegistro(){
             this.registro = true
         },
